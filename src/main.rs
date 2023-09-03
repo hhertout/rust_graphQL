d74f7c6c47020415c8816bab76f6244f5047824e
hhertout <hugues.hertout@gmail.com>
2023-09-03 18:35:19 +0200

    adding dotenvy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod api;
 mod config;
 mod graphql;
+mod services;
+mod schemas;
+mod entity;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     std::env::set_var("RUST_LOG", "info");
+    dotenvy::dotenv().ok();
     env_logger::init();
 
     api::start().await
